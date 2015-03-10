12f5652905ca3a98c572812e8aa29a75c0245294
unknown <tom.tang@H2600-0456L.perficient.com>
2015-03-10 11:03:50 +0800

    add company info

--- sites/all/themes/daughter/templates/company_profile/company_profile.tpl.php
+++ sites/all/themes/daughter/templates/company_profile/company_profile.tpl.php
@@ -1,21 +1,30 @@
-<?php ?>
+<?php 
+    $theme = drupal_get_path('theme','daughter');
+?>
 <div id="company_profile" >
     <div class="container">
         <div class='' >
-            <div class='col-md-5'  style="padding: 30px 0 30px 0">About/千金药业</div>
-            <div class='col-md-11 col-md-offset-1' style='height:500px;border: 1px solid;'></div>
+            <div class=' company_item_title'  style="">About/千金药业</div>
+            <div class='company_item_content' style='height:500px;border: 1px solid;'></div>
         </div>
         <div class='' style="">
-             <div class='col-md-3' style="padding: 30px 0 30px 0">About/千金药业</div>
-           <div class='col-md-11 col-md-offset-1' style='height:500px;border: 1px solid;'></div>
+             <div class='company_item_title' style="">About/千金静雅</div>
+           <div class='company_item_content' style='height:500px;border: 1px solid;'></div>
         </div>
          <div class='' style="">
-             <div class='col-md-3' style="padding: 30px 0 30px 0">About/千金药业</div>
-            <div class='col-md-11 col-md-offset-1' style='height:500px;border: 1px solid;'></div>
+            <div class='company_item_title' style="">About/企业风采</div>
+            <div class='company_item_content' style='height:500px;border: 1px solid;'></div>
         </div>
          <div class='' style="">
-             <div class='col-md-3' style="padding: 30px 0 30px 0">About/千金药业</div>
-           <div class='col-md-11 col-md-offset-1' style='height:500px;border: 1px solid;'></div>
+           <div class='company_item_title' style="">About/企业荣誉</div>
+           <div class='company_item_rongyu' >
+               <img style="margin-left:40px;" src="<?php print $theme?>/images/companyInfo/ry1.png" class="img-thumbnail img-responsive company_item_rongyu_img" alt="Responsive image">
+               <img src="<?php print $theme?>/images/companyInfo/ry2.png" class="img-thumbnail img-responsive company_item_rongyu_img" alt="Responsive image">
+               <img src="<?php print $theme?>/images/companyInfo/ry3.png" class="img-thumbnail img-responsive company_item_rongyu_img" alt="Responsive image">
+               <img src="<?php print $theme?>/images/companyInfo/ry4.png" class="img-thumbnail img-responsive company_item_rongyu_img" alt="Responsive image">
+               <img src="<?php print $theme?>/images/companyInfo/ry5.png" class="img-thumbnail img-responsive company_item_rongyu_img" alt="Responsive image">
+               
+           </div>
         </div>
     </div>
 </div> 
