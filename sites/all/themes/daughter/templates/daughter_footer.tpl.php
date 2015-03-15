d3abf5df96cfdd7093f9b33fba3b4fe6a0008180
unknown <jinnie@HS-JINREY.hs.handsome.com.cn>
2015-03-15 16:10:43 +0800

    jn

--- sites/all/themes/daughter/templates/daughter_footer.tpl.php
+++ sites/all/themes/daughter/templates/daughter_footer.tpl.php
@@ -6,18 +6,18 @@
                 <div ><h1 style="font-weight:bold;">给您医学级呵护</h1></div>
             </div>
              <div style="margin: 0 auto;color:white;">
-                <div style="width:150px;height: 2px;background-color: white;display: inline-block;margin-bottom: 10px; "></div>
+                <div style="width:100px;height: 2px;background-color: white;display: inline-block;margin-top: 3px; "></div>
             </div>
-            <div style="margin: 0 auto;color:white;font-weight:bold;">
-                <div >联系我们</div>
+            <div style="margin: 4px auto;color:white;font-weight:bold;">
+                <div><a href="/daughter/contactus">联系我们</a></div>
             </div>
             <div style="margin: 0 auto;color:white;height: 50px;    ">
                 <ul class="footer-links" style="width:100%;height:100%;padding-left: 0px;" >
-                    <li style="display:inline-block;">品牌理念</li>
+                    <li style="display:inline-block;"><a href="/daughter/companyprofile">品牌理念</a></li>
                     <li style="display:inline-block;"> | </li>
-                    <li style="display:inline-block;">企业介绍</li>
+                    <li style="display:inline-block;"><a href="/daughter/companyprofile">企业介绍</a></li>
                     <li style="display:inline-block;"> | </li>
-                    <li style="display:inline-block;">合作方式</li>
+                    <li style="display:inline-block;"><a href="/daughter/contactus">合作方式</a></li>
                 </ul>
             </div>
         </div>
--- sites/all/themes/daughter/templates/product/yimian.tpl.php
+++ sites/all/themes/daughter/templates/product/yimian.tpl.php
@@ -1,20 +1,29 @@
 <?php ?>
 <div >
-    <div class="yimian" style="padding-top: 50px;">
+    <div class="yimian">
         <div class="yimian-left">
             <div class="yimian-left-1"></div>
             <div class="yimian-left-2"></div>
             <div class="yimian-left-3"></div>
             <div class="yimian-left-4"></div>
+			<div class="yimian-left-5"></div>
+			<div class="yimian-left-6"></div>
+			<div class="yimian-left-7"></div>
         </div>
         <div class="yimian-right">
+			<div class="nav">
+				<img src="">
+			</div>
             <div class="yimian-right-1"></div>
             <div class="yimian-right-2"></div>
             <div class="yimian-right-3"></div>
-            <div class="yimian-right-4-1"></div>
-            <div class="yimian-right-4-2"></div>
-            <div class="yimian-right-4-3"></div>
-            <div class="yimian-right-4-r"></div>
+			<div class="yimian-right-4">
+				<div class="yimian-right-4_l">
+					<div class="yimian-right-4-1"></div>
+					<div class="yimian-right-4-2"></div>
+				</div>
+				<div class="yimian-right-4-r"></div>
+			</div>
         </div>
         
     </div>
